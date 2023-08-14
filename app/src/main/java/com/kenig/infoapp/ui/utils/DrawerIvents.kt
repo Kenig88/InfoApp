19a194e19037acaa9350ccd64d111f2f282176c9
s.krotov <14bassdrumand88@gmail.com>
2023-08-14 15:57:23 +0300

    2# events + assets

--- app/src/main/java/com/kenig/infoapp/ui/utils/DrawerIvents.kt
+++
@@ -1,3 +0,0 @@
-package com.kenig.infoapp.ui.utils
-
-sealed class DrawerIvents
---
+++ app/src/main/java/com/kenig/infoapp/ui/utils/ListItem.kt
@@ -0,0 +1,6 @@
+package com.kenig.infoapp.ui.utils
+
+data class ListItem(
+    val title: String,
+    val imageName: String
+)
